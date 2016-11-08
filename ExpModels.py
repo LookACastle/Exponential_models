9d008fb9bef95a112c579239574e82e3e5e54c4f
LookACastle <thorkik@gmail.com>
2016-11-08 13:07:06 +0100

    Added temp_data
    Added the data we're currently working with as a temporary, we should
    add support for adding your own data later.

--- ExpModels.py
+++ ExpModels.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+#import visual
+#import visual.graph
+import time # For optimisation and bugtesting purposes
+
+temp_data = set((15, 0), (16, 20), (18, 40), (19, 60), (22, 80), (24, 100), (31, 140), (42, 160), (49, 180), (67, 200), (78, 220) (90, 240), (105, 260), (109, 280), (122, 300), (125, 320))
+
+print(temp_data)
