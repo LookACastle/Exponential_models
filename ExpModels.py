056a46e31d7c4511a7aa375aff8c7cfb943e0a3a
LookACastle <thorkik@gmail.com>
2016-11-08 13:26:53 +0100

    The program finds the projection factor now
    Not cleaned up yet, but perfectly functional data is now acquired, yay
    :tada:.

--- ExpModels.py
+++ ExpModels.py
@@ -3,6 +3,14 @@
 #import visual.graph
 import time # For optimisation and bugtesting purposes
 
+start = time.time()
+
 temp_data = [15, 16, 18, 19, 22, 24, 31, 42, 49, 67, 78, 90, 105, 109, 122, 125]
 
-print(temp_data)
+for i in range(len(temp_data) - 1):
+    percent_increase = 100 / temp_data[i] * temp_data[i + 1] - 100
+    print(percent_increase)
+
+end = time.time()
+
+print("it took: ", end - start, "seconds to do this")
