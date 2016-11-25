aa5c71454d1e9b0c2b02cc1051c390651a165d5e
LookACastle <thorkik@gmail.com>
2016-11-25 11:15:25 +0100

    Removed the black box and made the checkbox text clearer.
    I removed the black box by setting it to 0*0px and the checkbox now says
    logarithmic toggle instead of logarithmic.

--- ExpModels.py
+++ ExpModels.py
@@ -1,7 +1,10 @@
 GlowScript 2.2 VPython
-from __future__ import division, print_function
 
 temp_data = [15, 16, 18, 19, 22, 24, 31, 42, 49, 67, 78, 90, 105, 109, 122, 125]
+
+# Makes the big black box disappear
+scene.width = 0
+scene.height = 0
 
 def get_projection_factor(list):
     output = []
@@ -11,11 +14,12 @@ def get_projection_factor(list):
     return output
 
 def graph_func(logarithmic):
-    test = graph(logy = logarithmic.checked, width = 600, height = 600, xmin = 0, ymin = 10)
+    g = graph(logy = logarithmic.checked, width = 600, height = 600, xmin = 0, ymin = 0)
 
     points = gdots(color = color.blue)
 
     for x in range(len(temp_data)):
         points.plot(x, temp_data[x])
 
-checkbox(bind = graph_func, text = "Logarithmic")
+
+checkbox(bind = graph_func, text = "Logarithmic toggle")
